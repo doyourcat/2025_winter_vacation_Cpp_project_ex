42802198c28670794e52a757bc36d1428d4c616d
doyourcat <yh9195844@gmail.com>
2026-02-24 01:36:38 +0900

    열혈 C++ 프로젝트 주석 수정

--- project10/AccountHandler.h
+++ project10/AccountHandler.h
@@ -6,21 +6,21 @@
 #include "Account.h"
 #include "BoundCheckArray.h"
 
-class AccountHandler
+class AccountHandler //게좌 관리 클래스
 {
 private:
-	BoundCheckArray<Account*> accArr;
-	int accNum;
+	BoundCheckArray<Account*> accArr; //템플릿 클래스로 배열 관리 Account 객체의 주소를 담아야함
+	int accNum; //새로운 계좌가 개설될 때, 빈 공간을 찾기 위한 인덱스 번호 역할(= 개설된 계좌가 몇 개인지 파악)
 public:
 	AccountHandler();
-	void ShowMenu(void) const;
-	void MakeAccount(void);
-	void DepositMoney(void);
-	void WithdrawMoney(void);
-	void ShowAllAccInfo(void) const;
-	~AccountHandler();
+	void ShowMenu(void) const; //메뉴 출력
+	void MakeAccount(void); //계좌 생성
+	void DepositMoney(void); //입금
+	void WithdrawMoney(void); //출금
+	void ShowAllAccInfo(void) const; //모든 정보 출력
+	~AccountHandler(); //동적 메모리 해제
 protected:
-	void MakeNormalAccount(void);
-	void MakeCreditAccount(void);
+	void MakeNormalAccount(void); //일반 계좌
+	void MakeCreditAccount(void); //신용 계좌
 };
 #endif
--- project10/BoundCheckArray.h
+++ project10/BoundCheckArray.h
@@ -3,15 +3,19 @@
 #ifndef __BOUND_CHECK_ARRAY_H__
 #define __BOUND_CHECK_ARRAY_H__
 
+#include "BankingCommonDecl.h"
+
 template <typename T>
 class BoundCheckArray
 {
 private:
-	T* arr;
-	int arrlen;
+	T* arr; //실제 데이터가 저장될 동적 배열의 시작 주소(템플릿 변수로 받음)
+	int arrlen;//배열의 최대 칸 기억하는 역할(나중에 범위를 벗어났는지 확인하는 데에 기준으로 쓰임)
 
-	BoundCheckArray(const BoundCheckArray& arr) {}
-	BoundCheckArray& operator=(const BoundCheckArray& arr) {}
+	//BoundCheckArray는 생성자에서 new Y[len]을 통해 힙 메모리를 빌려쓰는 클래스이기 때문에 외부에서 이 배열을 통째로 복사하려할 때에 복사를 못하게 막는 역할
+	//외부에서 복사하려할 때 컴파일러가 복사생성자(private) 때문에 복사를 못한다는 뜻으로 에러 뜨게 함
+	BoundCheckArray(const BoundCheckArray& arr) {} //복사 생성자
+	BoundCheckArray& operator=(const BoundCheckArray& arr) {} //대입 연산자
 
 public:
 	BoundCheckArray(int len = 100);
@@ -21,32 +25,32 @@ public:
 	~BoundCheckArray();
 };
 
-template <typename T>
+template <typename T> //template 함수 
 BoundCheckArray<T>::BoundCheckArray(int len) :arrlen(len)
 {
-	arr = new T[len];
+	arr = new T[len]; //템플릿 변수 타입의 배열을 len 크기만큼 힙 메모리에 생성
 }
 
 template <typename T>
 T& BoundCheckArray<T>::operator[] (int idx)
 {
-	if (idx < 0 || idx >= arrlen)
+	if (idx < 0 || idx >= arrlen) //idx가 0보다 작거나 배열 길이보다 크면 경고 후 프로그램 강제 종료
 	{
 		cout << "Array index out of bound exception" << endl;
 		exit(1);
 	}
-	return arr[idx];
+	return arr[idx]; //참조형(템플릿&) 반환->값 수정 가능
 }
 
 template <typename T>
 T BoundCheckArray<T>::operator[] (int idx) const
 {
-	if (idx < 0 || idx >= arrlen)
+	if (idx < 0 || idx >= arrlen) 
 	{
 		cout << "Array index out of bound exception" << endl;
 		exit(1);
 	}
-	return arr[idx];
+	return arr[idx]; //참조형(템플릿&) 반환->값 수정 불가능(=데이터 안 바뀜! ㅇㅅㅇ)
 }
 
 template <typename T>
@@ -58,7 +62,8 @@ int BoundCheckArray<T>::GetArrLen() const
 template <typename T>
 BoundCheckArray<T>::~BoundCheckArray()
 {
-	delete[]arr;
+	delete[]arr;  //BoundCheckArray<T>::BoundCheckArray(int len) :arrlen(len)에서 사용한 메모리를 해제
+
 }
 
 #endif
